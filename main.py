6c922a875cedbc3f87f958b8f58c9a9ac067270c
randy brown <thatrandybrown@gmail.com>
2022-07-19 23:30:24 -0500

    allow passing json as a value

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import sys
 import json
 import os
 
+def validateJSON(jsonData):
+    try:
+        json.loads(jsonData)
+    except ValueError as err:
+        return False
+    except TypeError as err:
+        return False
+    return True
+
 def build_json(json_items):
     obj = {}
     for item in json_items:
@@ -11,6 +20,8 @@ def build_json(json_items):
                 obj = {**obj, **json.loads(data)}
         else:
             prepared_item = item.split("=") if len(item.split("=")[1].split(",")) == 1 else [item.split("=")[0], item.split("=")[1].split(",")]
+            if validateJSON(prepared_item[1]):
+                prepared_item = [prepared_item[0], json.loads(prepared_item[1])]
             obj = {**obj, **dict([tuple(prepared_item)])}
     return json.dumps(obj)
 
