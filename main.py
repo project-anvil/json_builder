9fa7f06fa4fc138915820c4c7a4c4828ef815434
Randy Brown <thatrandybrown@gmail.com>
2023-10-03 21:23:39 -0500

    escaped strings shouldn't be split

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ def build_json(json_items):
             prepared_item = item.split("=") # an array of [key, value]
             if validateJSON(prepared_item[1]): # if the value is json
                 prepared_item = [prepared_item[0], json.loads(prepared_item[1])]
+# if item.split("=")[1] starts and ends with either " or ', then treat it as a single value
+            elif item.split("=")[1].startswith('"') and item.split("=")[1].endswith('"'):
+                prepared_item = [item.split("=")[0], item.split("=")[1][1:-1]]
             elif len(item.split("=")[1].split(",")) > 1: # if the value is a comma delimited list
                 prepared_item = [item.split("=")[0], item.split("=")[1].split(",")]
             obj = {**obj, **dict([tuple(prepared_item)])}
